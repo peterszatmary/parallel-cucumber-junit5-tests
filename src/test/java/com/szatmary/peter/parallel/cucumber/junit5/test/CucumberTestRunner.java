28f2d51724000918ab5330ca71a69c4a94167985
Peter Szatmary <peter.szatmary@gmail.com>
2021-07-27 19:05:45 +0200

    reports generation fixed

--- src/test/java/com/szatmary/peter/parallel/cucumber/junit5/test/CucumberTestRunner.java
+++ src/test/java/com/szatmary/peter/parallel/cucumber/junit5/test/CucumberTestRunner.java
@@ -1,12 +1,10 @@
 package com.szatmary.peter.parallel.cucumber.junit5.test;
 
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.platform.engine.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@CucumberOptions(plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json" })
 @Cucumber
 @SpringBootTest
 class CucumberTestRunner {
